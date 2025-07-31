d2d9123e8fa759318a46e17d0293d9f7aebbc749
Mujahid2000 <developermujahid2001@gmail.com>
2025-07-31 20:47:09 +0600

    add some code

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/', (res) => {
-  res.send("Hello World!")
-})
+
+app.get('/', (req, res) => {
+  res.send("<h1>Hello World!</h1>")
+});
 
 const startServer = async () => {
   try {
